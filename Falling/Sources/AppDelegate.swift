b8c5e3e91782a74e6e1be6a7ee6fcb47e0040012
Minny27 <dltmdals0608@naver.com>
2023-08-01 16:27:20 +0900

    [#3]Fix: logosplashLottie 파일 없음 에러 수정 - 변경된 로티 파일명으로 수정 - 런타임 시 print 코드들 삭제

--- Falling/Sources/AppDelegate.swift
+++ Falling/Sources/AppDelegate.swift
@@ -14,9 +14,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
-//        FirebaseApp.configure()
-      print("fasdjfalsjf")
         return true
     }
 
