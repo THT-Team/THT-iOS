37ca4e9c9e670e6e0cb3c54d943d6e2e8ef38396
kangho lee <ibcylon@naver.com>
2023-08-01 16:32:49 +0900

    change duplicate name for firebaseStorage

--- Falling/Sources/Foundation/AppData.swift
+++ Falling/Sources/Foundation/AppData.swift
@@ -6,3 +6,16 @@
 //
 
 import Foundation
+
+struct AppData {
+  private enum Key: String {
+    case access_token
+    case enable_auto_login
+  }
+
+  @DataStorage(key: Key.access_token.rawValue, defaultValue: "")
+  static var accessToken: String
+
+  @DataStorage(key: Key.enable_auto_login.rawValue, defaultValue: false)
+  static var isEnableAutoLogin: Bool
+}
