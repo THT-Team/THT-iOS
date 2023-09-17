18ad5582e1783300a4c3bbf5c52a17e96ba5599a
kangho lee <ibcylon@naver.com>
2023-09-17 21:21:11 +0900

    Refactor: chage topic padding and replace stackview

--- Falling/Sources/Foundation/Component/TFTopicBarView.swift
+++ Falling/Sources/Foundation/Component/TFTopicBarView.swift
@@ -10,6 +10,14 @@ import UIKit
 import SnapKit
 
 final class TFTopicBarView: TFBaseView {
+  private lazy var stackView: UIStackView = {
+    let stackView = UIStackView()
+    stackView.axis = .horizontal
+    stackView.spacing = 10
+    stackView.distribution = .fill
+    stackView.alignment = .center
+    return stackView
+  }()
   private lazy var titleLabel: TFPaddingLabel = {
     let label = TFPaddingLabel()
     label.layer.borderWidth = 1
@@ -25,7 +33,8 @@ final class TFTopicBarView: TFBaseView {
     let label = UILabel()
     label.textColor = FallingAsset.Color.neutral50.color
     label.font = UIFont.thtSubTitle2Sb
-    label.numberOfLines = 0
+    label.numberOfLines = 2
+    label.textAlignment = .left
     label.lineBreakMode = .byCharWrapping
     return label
   }()
@@ -55,26 +64,19 @@ final class TFTopicBarView: TFBaseView {
     self.backgroundColor = UIColor(named: "TopicBackground")
     self.layer.borderWidth = 1
     self.layer.borderColor = UIColor(named: "TopicBorder")?.cgColor
-    
-    self.addSubviews([titleLabel, contentLabel, closeButton])
-    
-    titleLabel.setContentHuggingPriority(.defaultLow + 1, for: .horizontal)
-    titleLabel.snp.makeConstraints {
-      $0.leading.equalToSuperview().offset(12)
-      $0.height.equalTo(35)
-      $0.centerY.equalToSuperview()
-    }
-    contentLabel.setContentHuggingPriority(.defaultLow, for: .horizontal)
-    contentLabel.snp.makeConstraints {
-      $0.centerY.equalToSuperview()
-      $0.top.bottom.equalToSuperview().inset(14)
-      $0.leading.equalTo(titleLabel.snp.trailing).offset(8)
+
+    stackView.addArrangedSubviews([titleLabel, contentLabel])
+    self.addSubviews([stackView, closeButton])
+
+    stackView.snp.makeConstraints {
+      $0.leading.equalToSuperview().offset(10)
+      $0.top.bottom.equalToSuperview().inset(8)
       $0.trailing.equalTo(closeButton.snp.leading).offset(-8)
     }
     closeButton.snp.makeConstraints {
       $0.size.equalTo(18)
       $0.centerY.equalToSuperview()
-      $0.trailing.equalToSuperview().inset(20)
+      $0.trailing.equalToSuperview().inset(14)
     }
   }
   
@@ -84,13 +86,6 @@ final class TFTopicBarView: TFBaseView {
     titleLabel.layer.masksToBounds = true
     titleLabel.layoutIfNeeded()
 
-    contentLabel.snp.remakeConstraints({
-      $0.centerY.equalToSuperview()
-      $0.top.bottom.equalToSuperview().inset(14)
-      $0.leading.equalTo(titleLabel.snp.trailing).offset(8)
-      $0.trailing.equalTo(closeButton.snp.leading).offset(-8)
-      $0.height.lessThanOrEqualTo(round(contentLabel.font.lineHeight) * 2)
-    })
   }
   
   func configure(title: String, content: String) {
