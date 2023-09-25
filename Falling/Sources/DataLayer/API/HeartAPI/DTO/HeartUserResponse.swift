a50fac27146b676c87f3389475b8e94508248551
kangho lee <ibcylon@naver.com>
2023-09-25 10:48:52 +0900

    Feat: Heart API add protocol type and stub data

---
+++ Falling/Sources/DataLayer/API/HeartAPI/DTO/HeartUserResponse.swift
@@ -0,0 +1,36 @@
+//
+//  HeartUserResponse.swift
+//  Falling
+//
+//  Created by Kanghos on 2023/09/17.
+//
+
+import Foundation
+
+// MARK: - UserResponse
+struct HeartUserResponse: Codable {
+    let username, userUUID: String
+    let age: Int
+    let introduction, address, phoneNumber, email: String
+    let idealTypeList, interestsList: [EmojiType]
+    let userProfilePhotos: [UserProfilePhoto]
+
+    enum CodingKeys: String, CodingKey {
+        case username
+        case userUUID = "userUuid"
+        case age, introduction, address, phoneNumber, email, idealTypeList, interestsList, userProfilePhotos
+    }
+}
+
+// MARK: - List
+struct EmojiType: Codable {
+    let idx: Int
+    let name, emojiCode: String
+}
+
+// MARK: - UserProfilePhoto
+struct UserProfilePhoto: Codable {
+    let url: String
+    let priority: Int
+}
+
