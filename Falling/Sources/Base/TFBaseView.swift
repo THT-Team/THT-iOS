e9ed33ec5ce83c9c77e18e40ba462c7154d17d9b
LeeSeungmin <dltmdals0608@naver.com>
2023-11-06 20:52:41 +0900

    [#44]Refactor: DimView base view에 추가
    - DimViewManager 삭제
    - self를 참조해서 hide를 하다보니 뷰 안에서만 hide를 할 수 있어서
    - base에 포함하게 됨.

--- Falling/Sources/Base/TFBaseView.swift
+++ Falling/Sources/Base/TFBaseView.swift
@@ -8,6 +8,11 @@
 import UIKit
 
 class TFBaseView: UIView {
+  lazy var dimView: UIView = {
+    let view = UIView()
+    view.backgroundColor = .clear
+    return view
+  }()
   
   override init(frame: CGRect) {
     super.init(frame: frame)
@@ -20,4 +25,24 @@ class TFBaseView: UIView {
   
   /// Set up configuration of view and add subviews
   func makeUI() { }
+    
+  func showDimView(frame: CGRect = UIWindow.keyWindow?.frame ?? .zero) {
+    dimView.frame = frame
+    DispatchQueue.main.async {
+      self.addSubview(self.dimView)
+      UIView.animate(withDuration: 0.0) {
+        self.dimView.backgroundColor =      FallingAsset.Color.cardShadow.color.withAlphaComponent(0.7)
+      }
+    }
+  }
+  
+  func hiddenDimView() {
+    DispatchQueue.main.async {
+      UIView.animate(withDuration: 0.0) { [weak self] in
+        self?.dimView.backgroundColor = .clear
+      } completion: { [weak self] _ in
+        self?.dimView.removeFromSuperview()
+      }
+    }
+  }
 }
