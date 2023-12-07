fae1d7742f50f0ea39ddbee1beec7bc177d3f939
kangho lee <ibcylon@naver.com>
2023-12-07 19:41:24 +0900

    SplashVC -> TFLaunchVC

--- Projects/Core/Src/BaseViewController/TFBaseViewController.swift
+++ Projects/Core/Src/BaseViewController/TFBaseViewController.swift
@@ -8,8 +8,11 @@
 import UIKit
 
 import DSKit
+import RxSwift
 
 open class TFBaseViewController: UIViewController, ViewControllable {
+  var disposeBag = DisposeBag()
+
   public override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
     super.init(nibName: nil, bundle: nil)
     TFLogger.ui.debug("\(#function) \(type(of: self))")
@@ -17,8 +20,12 @@ open class TFBaseViewController: UIViewController, ViewControllable {
 
   open override func viewDidLoad() {
     super.viewDidLoad()
-
+    
     TFLogger.ui.debug("\(#function) \(type(of: self))")
+    self.view.backgroundColor = DSKitAsset.Color.neutral700.color
+    makeUI()
+    bindViewModel()
+    navigationSetting()
   }
 
   required public init?(coder: NSCoder) {
@@ -27,5 +34,25 @@ open class TFBaseViewController: UIViewController, ViewControllable {
 
   deinit {
     TFLogger.ui.debug("\(#function) \(type(of: self))")
+  }
+
+  // MARK: - Open
+  open func makeUI() { }
+
+  open func bindViewModel() { }
+
+  open func navigationSetting() {
+    navigationController?.navigationBar.topItem?.title = ""
+    navigationController?.navigationBar.backIndicatorImage = DSKitAsset.Image.chevron.image
+    navigationController?.navigationBar.backIndicatorTransitionMaskImage = DSKitAsset.Image.chevron.image
+    navigationController?.navigationBar.tintColor = DSKitAsset.Color.neutral50.color
+
+    let navBarAppearance = UINavigationBarAppearance()
+    navBarAppearance.titlePositionAdjustment.horizontal = -CGFloat.greatestFiniteMagnitude
+    navBarAppearance.titleTextAttributes = [.font: UIFont.thtH4Sb, .foregroundColor: DSKitAsset.Color.neutral50.color]
+    navBarAppearance.backgroundColor = DSKitAsset.Color.neutral700.color
+    navBarAppearance.shadowColor = nil
+    navigationItem.standardAppearance = navBarAppearance
+    navigationItem.scrollEdgeAppearance = navBarAppearance
   }
 }
