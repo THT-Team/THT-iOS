2fe945c9c90f83a588a596f0284bdb4d5f7809b4
kangho lee <ibcylon@naver.com>
2023-12-12 20:44:19 +0900

    Feat: SignUp Demo

---
+++ Projects/Features/SignUp/Demo/Src/Coordinator/AppBuilder.swift
@@ -0,0 +1,38 @@
+//
+//  AppRootBuilder.swift
+//  Feature
+//
+//  Created by Kanghos on 2023/12/06.
+//
+
+import UIKit
+
+import Core
+
+import SignUp
+import SignUpInterface
+
+public protocol AppRootBuildable {
+  func build() -> LaunchCoordinating
+}
+
+public final class AppRootBuilder: AppRootBuildable {
+  public init() { }
+
+  lazy var signUpBuildable: SignUpBuildable = {
+    SignUpBuilder()
+  }()
+
+  public func build() -> LaunchCoordinating {
+
+    let viewController = NavigationViewControllable(rootViewControllable: TFLaunchViewController())
+
+    let coordinator = AppCoordinator(
+      viewControllable: viewController,
+      signUpBuildable: self.signUpBuildable
+    )
+
+    return coordinator
+  }
+}
+
