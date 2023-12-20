0c9481b4221533ed24d92cf625057387b5db6307
kangho lee <ibcylon@naver.com>
2023-12-20 21:33:31 +0900

    Add: Networks Module

---
+++ Projects/Modules/Network/Src/Network/Networkable.swift
@@ -0,0 +1,31 @@
+//
+//  Networkable.swift
+//  Falling
+//
+//  Created by Kanghos on 2023/07/10.
+//
+
+import Moya
+
+public protocol Networkable {
+
+  // TargetType을 준수하는 Generic Type
+  associatedtype Target: TargetType
+
+  // DIP를 위해 protocol에 provider객체를 만드는 함수 정의
+  static func makeProvider() -> MoyaProvider<Target>
+}
+
+public extension Networkable {
+
+  static func makeProvider() -> MoyaProvider<Target> {
+
+    let loggerPlugin = NetworkLoggerPlugin()
+    return MoyaProvider<Target>(plugins: [loggerPlugin])
+  }
+
+  static var provider: MoyaProvider<Target> {
+    makeProvider()
+  }
+}
+
