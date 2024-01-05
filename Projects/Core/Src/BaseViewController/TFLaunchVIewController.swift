8b712c0b1baa10ab1d19b017a70e99aca2c0cedc
LeeSeungmin <dltmdals0608@naver.com>
2024-01-05 14:38:21 +0900

    Refactor: 스플래시 뷰의 슈퍼 뷰 배경 색 설정

--- Projects/Core/Src/BaseViewController/TFLaunchVIewController.swift
+++ Projects/Core/Src/BaseViewController/TFLaunchVIewController.swift
@@ -14,15 +14,17 @@ import DSKit
 
 public final class TFLaunchViewController: TFBaseViewController {
   private lazy var splashLottieView = LottieAnimationView(animation: AnimationAsset.logoSplash.animation)
-
+  
   public override func loadView() {
     super.loadView()
+    view.backgroundColor = DSKitAsset.Color.neutral700.color
+    
     self.view.addSubview(splashLottieView)
     splashLottieView.snp.makeConstraints {
       $0.center.equalToSuperview()
       $0.height.width.equalTo(view.bounds.height * 0.7)
     }
-
+    
     splashLottieView.play()
   }
 }
