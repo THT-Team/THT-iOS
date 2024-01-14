ba7298a60e7ca9340567158a10ad8c4127611db7
LeeSeungmin <dltmdals0608@naver.com>
2024-01-14 22:44:08 +0900

    Update: Reactive 확장
    - view will disappear 추가

--- Projects/Core/Src/Util/Rx+Util.swift
+++ Projects/Core/Src/Util/Rx+Util.swift
@@ -11,20 +11,25 @@ import RxCocoa
 import RxSwift
 
 public extension Reactive where Base: UIViewController {
-    var viewDidLoad: ControlEvent<Void> {
-        let source = methodInvoked(#selector(Base.viewDidLoad)).map { _ in }
-        return ControlEvent(events: source)
-    }
-
-    var viewWillAppear: ControlEvent<Bool> {
-        let source = methodInvoked(#selector(Base.viewWillAppear)).map { $0.first as? Bool ?? false }
-        return ControlEvent(events: source)
-    }
-
-    var viewDidAppear: ControlEvent<Bool> {
-        let source = methodInvoked(#selector(Base.viewDidAppear)).map { $0.first as? Bool ?? false }
-        return ControlEvent(events: source)
-    }
+  var viewDidLoad: ControlEvent<Void> {
+    let source = methodInvoked(#selector(Base.viewDidLoad)).map { _ in }
+    return ControlEvent(events: source)
+  }
+  
+  var viewWillAppear: ControlEvent<Bool> {
+    let source = methodInvoked(#selector(Base.viewWillAppear)).map { $0.first as? Bool ?? false }
+    return ControlEvent(events: source)
+  }
+  
+  var viewDidAppear: ControlEvent<Bool> {
+    let source = methodInvoked(#selector(Base.viewDidAppear)).map { $0.first as? Bool ?? false }
+    return ControlEvent(events: source)
+  }
+  
+  var viewWillDisAppear: ControlEvent<Bool> {
+    let source = methodInvoked(#selector(Base.viewWillDisappear)).map { $0.first as? Bool ?? false }
+    return ControlEvent(events: source)
+  }
 }
 
 public extension ObservableType {
