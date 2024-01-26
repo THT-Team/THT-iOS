1c723f15b648477e6949fad89fd1e0f5d3de9f77
LeeSeungmin <dltmdals0608@naver.com>
2024-01-27 00:01:48 +0900

    Update: profile url 변경
    - 비율이 다른 이미지 링크로 변경

--- Projects/Data/Src/Repository/Falling/FallingTarget+SampleData.swift
+++ Projects/Data/Src/Repository/Falling/FallingTarget+SampleData.swift
@@ -59,11 +59,11 @@ extension FallingTarget {
               ],
               "userProfilePhotos": [
                 {
-                  "url": "test1",
+                  "url": "https://cdn.pixabay.com/photo/2024/01/15/21/16/dog-8510901_640.jpg",
                   "priority": 1
                 },
                 {
-                  "url": "https://firebasestorage.googleapis.com/v0/b/tht-falling.appspot.com/o/images%2Fprofile_example2.png?alt=media&token=e19493ed-10ba-4784-bf94-f4b99af84161",
+                  "url": "https://firebasestorage.googleapis.com/v0/b/tht-falling.appspot.com/o/images%2Fprofile_example1.png?alt=media&token=dc28c0cd-98b2-4332-9660-35530283d77c",
                   "priority": 2
                 },
                 {
@@ -116,11 +116,11 @@ extension FallingTarget {
                             ],
                             "userProfilePhotos": [
                               {
-                                "url": "https://firebasestorage.googleapis.com/v0/b/tht-falling.appspot.com/o/images%2Fprofile_example1 .png?alt=media&token=dc28c0cd-98b2-4332-9660-35530283d77c",
+                                "url": "https://cdn.pixabay.com/photo/2023/05/07/11/57/surfboard-7976219_960_720.jpg",
                                 "priority": 1
                               },
                               {
-                                "url": "https://firebasestorage.googleapis.com/v0/b/tht-falling.appspot.com/o/images%2Fprofile_example2.png?alt=media&token=e19493ed-10ba-4784-bf94-f4b99af84161",
+                                "url": "https://cdn.pixabay.com/photo/2024/01/07/11/17/welsh-corgi-8492879_640.jpg",
                                 "priority": 2
                               },
                               {
@@ -173,11 +173,11 @@ extension FallingTarget {
               ],
               "userProfilePhotos": [
                 {
-                  "url": "https://firebasestorage.googleapis.com/v0/b/tht-falling.appspot.com/o/images%2Fprofile_example1.png?alt=media&token=dc28c0cd-98b2-4332-9660-35530283d77c",
+                  "url": "https://cdn.pixabay.com/photo/2023/03/18/05/26/ferris-wheel-7859855_640.jpg",
                   "priority": 1
                 },
                 {
-                  "url": "https://firebasestorage.googleapis.com/v0/b/tht-falling.appspot.com/o/images%2Fprofile_example2.png?alt=media&token=e19493ed-10ba-4784-bf94-f4b99af84161",
+                  "url": "https://cdn.pixabay.com/photo/2023/11/17/12/46/cat-8394224_640.jpg",
                   "priority": 2
                 },
                 {
