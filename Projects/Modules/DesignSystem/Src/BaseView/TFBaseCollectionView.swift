d272ae16211059ac602c6fe78726a89d233656fc
LeeSeungmin <dltmdals0608@naver.com>
2024-03-01 22:05:05 +0900

    Enhance: 프로파일 컬렉션 뷰에 dimView를 올리도록 수정

---
+++ Projects/Modules/DesignSystem/Src/BaseView/TFBaseCollectionView.swift
@@ -0,0 +1,39 @@
+//
+//  TFBaseCollectionView.swift
+//  DSKit
+//
+//  Created by SeungMin on 3/1/24.
+//
+
+import UIKit
+
+open class TFBaseCollectionView: UICollectionView {
+  private let dimView: UIView = {
+    let view = UIView()
+    view.backgroundColor = DSKitAsset.Color.DimColor.default.color
+    return view
+  }()
+  
+  public func showDimView(frame: CGRect = UIWindow.keyWindow?.frame ?? .zero) {
+    dimView.frame = frame
+    DispatchQueue.main.async { [weak self] in
+      guard let self = self else { return }
+      self.addSubview(self.dimView)
+      UIView.animate(withDuration: 0.0) {
+        self.dimView.backgroundColor = DSKitAsset.Color.DimColor.default.color
+      }
+    }
+  }
+  
+  public func hiddenDimView() {
+    DispatchQueue.main.async {
+      UIView.animate(withDuration: 0.0) { [weak self] in
+        guard let self = self else { return }
+        self.dimView.backgroundColor = DSKitAsset.Color.clear.color
+      } completion: { [weak self] _ in
+        guard let self = self else { return }
+        self.dimView.removeFromSuperview()
+      }
+    }
+  }
+}
