67ac4c0a38cab788581fcf857c07fd96e50355b9
Will <dltmdals0608@naver.com>
2024-10-06 18:23:01 +0900

    Fix: MyPage demo에 런타임 에러 수정
    - demo app에 UserDomainUseCaseInterface 주입

--- Projects/Features/MyPage/Demo/Src/AppDelegate+Register.swift
+++ Projects/Features/MyPage/Demo/Src/AppDelegate+Register.swift
@@ -14,6 +14,7 @@ import SignUpInterface
 import MyPage
 import MyPageInterface
 
+import Domain
 import Data
 
 extension AppDelegate {
@@ -57,6 +58,12 @@ extension AppDelegate {
         )
       }
     )
+    
+    container.register(
+      interface: UserDomainUseCaseInterface.self,
+      implement: {
+        DefaultUserDomainUseCase(repository: DefaultUserDomainRepository())
+      })
   }
 }
 
