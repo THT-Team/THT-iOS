02d4b6e54ae1cbc8fb5b7e4dedd27fe1f1b98267
kangho lee <ibcylon@naver.com>
2025-01-04 19:08:56 +0900

    Fix: xPosition sync to Figma

--- Projects/Core/Src/Transition/RotateTransition.swift
+++ Projects/Core/Src/Transition/RotateTransition.swift
@@ -27,11 +27,13 @@ public final class RotatateTransition: NSObject, UIViewControllerAnimatedTransit
 
     let fromView = from.view
 
-    let transfrom = CGAffineTransform(rotationAngle: -.pi / 8)
-      .translatedBy(x: -100, y: 0)
+    let width = transitionContext.containerView.frame.width
+    let xPosition: CGFloat = width * -170 / 390
+    let transfrom = CGAffineTransform(rotationAngle: -.pi / 12)
+      .translatedBy(x: xPosition, y: 0)
     UIViewPropertyAnimator.runningPropertyAnimator(withDuration: duration, delay: 0, options: [.curveEaseOut]) {
       fromView?.transform = transfrom
-      fromView?.alpha = 0.1
+      fromView?.alpha = 0.5
     } completion: { postion in
       let finished = !transitionContext.transitionWasCancelled
       transitionContext.completeTransition(finished)
