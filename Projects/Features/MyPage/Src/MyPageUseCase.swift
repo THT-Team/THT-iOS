27b898cb9e066228051e60ffb0de7110da4e5b64
kangho lee <ibcylon@naver.com>
2025-02-21 17:59:04 +0900

    Fix: MySetting Webview URL

--- Projects/Features/MyPage/Src/MyPageUseCase.swift
+++ Projects/Features/MyPage/Src/MyPageUseCase.swift
@@ -22,8 +22,7 @@ public final class MyPageUseCase: MyPageUseCaseInterface {
     let userSignUpInfo = UserDefaultRepository.shared.fetchModel(for: .sign_up_info, type: UserSignUpInfoRes.self)
     
     return [
-      createAccountMenus(signUpInfo: userSignUpInfo, user: user)
-      ,
+      createAccountMenus(signUpInfo: userSignUpInfo, user: user),
       SectionModel(
         type: .activity,
         items: [.item(MySetting.Item(title: "저장된 연락처 차단하기"))]),
@@ -36,16 +35,16 @@ public final class MyPageUseCase: MyPageUseCaseInterface {
       SectionModel(
         type: .support,
         items: [
-          .linkItem(MySetting.LinkItem(title: "자주 묻는 질문", url: URL(string: "https://www.notion.so/janechoi/46bf7dbf13da4ca5a2e8dc44a5fb9236?pvs=4")!)),
+          .linkItem(MySetting.LinkItem(title: "자주 묻는 질문", url: URL(string: "https://janechoi.notion.site/46bf7dbf13da4ca5a2e8dc44a5fb9236?pvs=4")!)),
           .item(MySetting.Item(title: "문의 및 피드백 보내기"))]),
       SectionModel(
         type: .law,
         items: [
-          .linkItem(MySetting.LinkItem(title: "서비스 이용약관", url: URL(string: "https://www.notion.so/janechoi/46bf7dbf13da4ca5a2e8dc44a5fb9236?pvs=4")!)),
-          .linkItem(MySetting.LinkItem(title: "개인정보 처리방침", url: URL(string: "https://www.notion.so/janechoi/46bf7dbf13da4ca5a2e8dc44a5fb9236?pvs=4")!)),
-          .linkItem(MySetting.LinkItem(title: "위치정보 이용약관", url: URL(string: "https://www.notion.so/janechoi/46bf7dbf13da4ca5a2e8dc44a5fb9236?pvs=4")!)),
-          .linkItem(MySetting.LinkItem(title: "라이센스", url: URL(string: "https://www.notion.so/janechoi/46bf7dbf13da4ca5a2e8dc44a5fb9236?pvs=4")!)),
-          .linkItem(MySetting.LinkItem(title: "사업자 정보", url: URL(string: "http://www.ftc.go.kr/bizCommPop.do?wrkr_no=6806400545")!))]),
+          .linkItem(MySetting.LinkItem(title: "서비스 이용약관", url: URL(string: "https://janechoi.notion.site/526c51e9cb584f29a7c16251914bb3cb?pvs=4")!)),
+          .linkItem(MySetting.LinkItem(title: "개인정보 처리방침", url: URL(string: "https://janechoi.notion.site/5923a3c20259459bbacaff41290fc615?pvs=4")!)),
+          .linkItem(MySetting.LinkItem(title: "위치정보 이용약관", url: URL(string: "https://janechoi.notion.site/b45cf5f24d39494daf0a03b907a2ab7d?pvs=4")!)),
+          .linkItem(MySetting.LinkItem(title: "라이센스", url: URL(string: "https://janechoi.notion.site/c424bc053e7a479daa595d8bd69b0d1f?pvs=4")!)),
+          .linkItem(MySetting.LinkItem(title: "사업자 정보", url: URL(string: "https://janechoi.notion.site/4f20dcc5c4084318910edfee3db5edb3?pvs=4")!))]),
       SectionModel(
         type: .accoutSetting,
         items: [.item(MySetting.Item(title: "계정 설정"))]),
