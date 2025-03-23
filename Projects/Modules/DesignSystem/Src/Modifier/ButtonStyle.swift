58cc8cb41c8b9c2d0b12c93796c3e0f1aaa70550
Will <dltmdals0608@naver.com>
2025-03-23 23:50:17 +0900

    Add: 버튼 스타일 추가 및 Color의 hex parameter init 추가

---
+++ Projects/Modules/DesignSystem/Src/Modifier/ButtonStyle.swift
@@ -0,0 +1,40 @@
+//
+//  ButtonStyle.swift
+//  DSKit
+//
+//  Created by SeungMin on 3/23/25.
+//
+
+import SwiftUI
+
+public struct SubmitButtonStyle: ButtonStyle {
+  @Environment(\.isEnabled) var isEnabled
+  
+  public func makeBody(configuration: ButtonStyle.Configuration) -> some SwiftUI.View {
+    
+    configuration.label
+      .font(weight: 700, size: 17, lineSpacingPercent: 130)
+      .foregroundStyle(Color.neutral900)
+      .frame(maxWidth: .infinity)
+      .frame(height: 56)
+      .background {
+        RoundedRectangle(cornerRadius: 8)
+          .fill(isEnabled ? Color(hex: "#F9CC2E") : Color(hex: "#F9CC2E1A"))
+      }
+      .opacity(configuration.isPressed ? 0.3 : 1)
+  }
+}
+
+extension ButtonStyle where Self == SubmitButtonStyle {
+  public static var submit: SubmitButtonStyle {
+    SubmitButtonStyle()
+  }
+}
+
+#Preview {
+  Button("버튼") {
+    
+  }
+  .buttonStyle(.submit)
+  .padding(.horizontal, 16)
+}
--- Projects/Modules/DesignSystem/Src/Util/Color+Util.swift
+++ Projects/Modules/DesignSystem/Src/Util/Color+Util.swift
@@ -62,3 +62,26 @@ extension Color {
   public static let unSelected = DSKitAsset.Color.unSelected.swiftUIColor
   public static let blur = DSKitAsset.Color.blur.swiftUIColor
 }
+
+extension UIColor {
+  public convenience init(hex: String) {
+    var hexSanitized = hex.trimmingCharacters(in: .whitespacesAndNewlines)
+    hexSanitized = hexSanitized.replacingOccurrences(of: "#", with: "")
+    
+    var rgb: UInt64 = 0
+    Scanner(string: hexSanitized).scanHexInt64(&rgb)
+    
+    let r = CGFloat((rgb & 0xFF0000) >> 16) / 255.0
+    let g = CGFloat((rgb & 0x00FF00) >> 8) / 255.0
+    let b = CGFloat(rgb & 0x0000FF) / 255.0
+    
+    self.init(red: r, green: g, blue: b, alpha: 1.0)
+  }
+}
+
+extension Color {
+  public init(hex: String) {
+    let uiColor = UIColor(hex: hex)
+    self.init(uiColor)
+  }
+}
