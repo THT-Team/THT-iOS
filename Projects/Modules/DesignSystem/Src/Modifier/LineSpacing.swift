41315b2a7e89e2963e85abb737624cf133f50781
Will <dltmdals0608@naver.com>
2025-03-23 22:28:31 +0900

    Add: SwiftUI 기반 lineSpacing 설정 함수 및 color 설정

---
+++ Projects/Modules/DesignSystem/Src/Modifier/LineSpacing.swift
@@ -0,0 +1,131 @@
+//
+//  LineSpacing.swift
+//  DSKit
+//
+//  Created by SeungMin on 3/23/25.
+//
+
+import Foundation
+import SwiftUI
+
+public struct PretendardFontModifier: ViewModifier {
+  var weight: AppFont.Pretendard.Weight
+  var size: CGFloat
+  var lineSpacingPercent: CGFloat
+  var letterSpacingPercent: CGFloat
+  
+  public init(
+    weight: AppFont.Pretendard.Weight,
+    size: CGFloat,
+    lineSpacingPercent: CGFloat,
+    letterSpacingPercent: CGFloat = -2
+  ) {
+    self.weight = weight
+    self.size = size
+    self.lineSpacingPercent = lineSpacingPercent
+    self.letterSpacingPercent = letterSpacingPercent
+  }
+  
+  public init(
+    weight: Int,
+    size: CGFloat,
+    lineSpacingPercent: CGFloat,
+    letterSpacingPercent: CGFloat = -2
+  ) {
+    self.weight = .init(rawValue: weight)!
+    self.size = size
+    self.lineSpacingPercent = lineSpacingPercent
+    self.letterSpacingPercent = letterSpacingPercent
+  }
+  
+  public func body(content: Content) -> some SwiftUI.View {
+    let lineHeight = size * (lineSpacingPercent / 100)
+    
+    content
+      .font(.pretendard(weight: weight, size: size))
+      .lineSpacing(lineHeight - size)
+      .tracking(size * (letterSpacingPercent / 100))
+      .padding(.vertical, (lineHeight - size) / 2)
+  }
+}
+
+public extension SwiftUI.View {
+  func font(
+    weight: AppFont.Pretendard.Weight,
+    size: CGFloat,
+    lineSpacingPercent: CGFloat,
+    letterSpacingPercent: CGFloat = -2
+  ) -> some SwiftUI.View {
+    modifier(
+      PretendardFontModifier(
+        weight: weight,
+        size: size,
+        lineSpacingPercent: lineSpacingPercent,
+        letterSpacingPercent: letterSpacingPercent
+      )
+    )
+  }
+  
+  func font(
+    weight: Int,
+    size: CGFloat,
+    lineSpacingPercent: CGFloat,
+    letterSpacingPercent: CGFloat = -2
+  ) -> some SwiftUI.View {
+    modifier(
+      PretendardFontModifier(
+        weight: weight,
+        size: size,
+        lineSpacingPercent: lineSpacingPercent,
+        letterSpacingPercent: letterSpacingPercent
+      )
+    )
+  }
+}
+
+public enum AppFont {
+  public enum Pretendard {
+    public enum Weight: Int {
+//      case thin = 100
+//      case extraLight = 200
+//      case light = 300
+      case regular = 400
+      case medium = 500
+      case semibold = 600
+      case bold = 700
+      case extrabold = 800
+//      case heavy = 900
+      
+      var name: String {
+        switch self {
+//        case .thin: "Thin"
+//        case .extraLight: "ExtraLight"
+//        case .light: "Light"
+        case .regular: DSKitFontFamily.Pretendard.regular.name
+        case .medium: DSKitFontFamily.Pretendard.medium.name
+        case .semibold: DSKitFontFamily.Pretendard.semiBold.name
+        case .bold: DSKitFontFamily.Pretendard.bold.name
+        case .extrabold: DSKitFontFamily.Pretendard.extraBold.name
+//        case .heavy: "Heavy"
+        }
+      }
+    }
+  }
+}
+
+
+public extension Font {
+  static func pretendard(
+    weight: AppFont.Pretendard.Weight,
+    size: CGFloat
+  ) -> Font {
+    return .custom(weight.name, size: size)
+  }
+  
+  static func pretendard(
+    weight: Int,
+    size: CGFloat
+  ) -> Font {
+    return .custom(AppFont.Pretendard.Weight(rawValue: weight)!.name, size: size)
+  }
+}
---
+++ Projects/Modules/DesignSystem/Src/Util/Color+Util.swift
@@ -0,0 +1,64 @@
+//
+//  Color+Util.swift
+//  DSKit
+//
+//  Created by SeungMin on 3/23/25.
+//
+
+import SwiftUI
+
+extension Color {
+  public static let cardShadow = DSKitAsset.Color.cardShadow.swiftUIColor
+  public static let chatTopicBackground = DSKitAsset.Color.chatTopicBackground.swiftUIColor
+  public static let chatTopicBorder = DSKitAsset.Color.chatTopicBorder.swiftUIColor
+  public static let clear = DSKitAsset.Color.clear.swiftUIColor
+  public enum DimColor: Sendable {
+    public static let `default` = DSKitAsset.Color.DimColor.default.swiftUIColor
+    public static let loading = DSKitAsset.Color.DimColor.loading.swiftUIColor
+    public static let pauseDim = DSKitAsset.Color.DimColor.pauseDim.swiftUIColor
+    public static let signUpDim = DSKitAsset.Color.DimColor.signUpDim.swiftUIColor
+    public static let timerDim = DSKitAsset.Color.DimColor.timerDim.swiftUIColor
+  }
+  public static let disabled = DSKitAsset.Color.disabled.swiftUIColor
+  public enum DummyUserGradient: Sendable {
+    public static let backgroundFirst = DSKitAsset.Color.DummyUserGradient.backgroundFirst.swiftUIColor
+    public static let backgroundSecond = DSKitAsset.Color.DummyUserGradient.backgroundSecond.swiftUIColor
+    public static let borderFirst = DSKitAsset.Color.DummyUserGradient.borderFirst.swiftUIColor
+    public static let borderSecond = DSKitAsset.Color.DummyUserGradient.borderSecond.swiftUIColor
+  }
+  public static let error = DSKitAsset.Color.error.swiftUIColor
+  public static let event = DSKitAsset.Color.event.swiftUIColor
+  public static let linear0 = DSKitAsset.Color.linear0.swiftUIColor
+  public static let linear1 = DSKitAsset.Color.linear1.swiftUIColor
+  public static let linear2 = DSKitAsset.Color.linear2.swiftUIColor
+  public enum LikeGradient: Sendable {
+    public static let gradientFirst = DSKitAsset.Color.LikeGradient.gradientFirst.swiftUIColor
+    public static let gradientSecond = DSKitAsset.Color.LikeGradient.gradientSecond.swiftUIColor
+    public static let gradientThird = DSKitAsset.Color.LikeGradient.gradientThird.swiftUIColor
+  }
+  public static let neutral300 = DSKitAsset.Color.neutral300.swiftUIColor
+  public static let neutral400 = DSKitAsset.Color.neutral400.swiftUIColor
+  public static let neutral450 = DSKitAsset.Color.neutral450.swiftUIColor
+  public static let neutral50 = DSKitAsset.Color.neutral50.swiftUIColor
+  public static let neutral500 = DSKitAsset.Color.neutral500.swiftUIColor
+  public static let neutral600 = DSKitAsset.Color.neutral600.swiftUIColor
+  public static let neutral700 = DSKitAsset.Color.neutral700.swiftUIColor
+  public static let neutral900 = DSKitAsset.Color.neutral900.swiftUIColor
+  public static let pauseTitle = DSKitAsset.Color.pauseTitle.swiftUIColor
+  public static let payment = DSKitAsset.Color.payment.swiftUIColor
+  public static let primary300 = DSKitAsset.Color.primary300.swiftUIColor
+  public static let primary400 = DSKitAsset.Color.primary400.swiftUIColor
+  public static let primary500 = DSKitAsset.Color.primary500.swiftUIColor
+  public static let primary600 = DSKitAsset.Color.primary600.swiftUIColor
+  public static let kakaoPrimary = DSKitAsset.Color.kakaoPrimary.swiftUIColor
+  public static let naverPrimary = DSKitAsset.Color.naverPrimary.swiftUIColor
+  public static let thtOrange100 = DSKitAsset.Color.thtOrange100.swiftUIColor
+  public static let thtOrange200 = DSKitAsset.Color.thtOrange200.swiftUIColor
+  public static let thtOrange300 = DSKitAsset.Color.thtOrange300.swiftUIColor
+  public static let thtOrange400 = DSKitAsset.Color.thtOrange400.swiftUIColor
+  public static let thtRed = DSKitAsset.Color.thtRed.swiftUIColor
+  public static let topicBackground = DSKitAsset.Color.topicBackground.swiftUIColor
+  public static let topicBorder = DSKitAsset.Color.topicBorder.swiftUIColor
+  public static let unSelected = DSKitAsset.Color.unSelected.swiftUIColor
+  public static let blur = DSKitAsset.Color.blur.swiftUIColor
+}
