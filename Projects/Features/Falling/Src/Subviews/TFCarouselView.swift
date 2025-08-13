6341db9d1a9b5c44622875ddfa85710823089eef
Will <dltmdals0608@naver.com>
2025-08-14 01:31:53 +0900

    Refactor: page control dot 크기 수정

--- Projects/Features/Falling/Src/Subviews/TFCarouselView.swift
+++ Projects/Features/Falling/Src/Subviews/TFCarouselView.swift
@@ -12,36 +12,45 @@ import UIKit
 import DSKit
 
 final class TFCarouselView: TFBaseView {
-
+  
   lazy var carouselView = TFBaseCollectionView(frame: .zero, collectionViewLayout: createLayout())
-
+  
   lazy var pageControl = UIPageControl().then {
-    $0.currentPageIndicatorTintColor = .black
-    $0.pageIndicatorTintColor = .gray
+    let normalDot = circleImage(diameter: 6, color: DSKitAsset.Color.neutral50.color)
+    let currentDot = circleImage(diameter: 6, color: DSKitAsset.Color.neutral300.color)
+    
+    $0.preferredIndicatorImage = normalDot
+    
+    guard $0.numberOfPages > 0 else { return }
+    
+    for i in 0..<$0.numberOfPages {
+      $0.setIndicatorImage(normalDot, forPage: i)
+    }
+    $0.setIndicatorImage(currentDot, forPage: $0.currentPage)
   }
-
+  
   func register<T: UICollectionViewCell>(cellType: T.Type) {
     carouselView.register(cellType: cellType.self)
   }
-
+  
   override func makeUI() {
     carouselView.isScrollEnabled = false
-
+    
     addSubviews(carouselView, pageControl)
-
+    
     carouselView.snp.makeConstraints {
       $0.edges.equalToSuperview()
     }
-
+    
     pageControl.snp.makeConstraints {
       $0.centerX.equalToSuperview()
-      $0.height.equalTo(30)
-      $0.bottom.equalToSuperview().inset(10)
+      $0.height.equalTo(6)
+      $0.bottom.equalToSuperview().inset(12)
     }
   }
-
+  
   // MARK: pageControl
-  func createLayout() -> UICollectionViewLayout {
+  private func createLayout() -> UICollectionViewLayout {
     let sectionLayout: NSCollectionLayoutSection = .horizontalListSection()
     sectionLayout.visibleItemsInvalidationHandler = { [weak self] (items, offset, environment) in
       guard let self = self else { return }
@@ -49,5 +58,13 @@ final class TFCarouselView: TFBaseView {
       self.pageControl.currentPage = index
     }
     return UICollectionViewCompositionalLayout(section: sectionLayout)
+  }
+  
+  private func circleImage(diameter: CGFloat, color: UIColor) -> UIImage {
+    let r = UIGraphicsImageRenderer(size: CGSize(width: diameter, height: diameter))
+    return r.image { ctx in
+      color.setFill()
+      UIBezierPath(ovalIn: CGRect(x: 0, y: 0, width: diameter, height: diameter)).fill()
+    }.withRenderingMode(.alwaysOriginal)
   }
 }
