0bc2be440669b80437147fd116cbb44ebbe7b16c
Will <dltmdals0608@naver.com>
2025-08-17 23:21:03 +0900

    Fix: multiple 버그 수정
    - provisioning 고정 시켰던 거 롤백

--- Tuist/ProjectDescriptionHelpers/Helper/AppConfiguration.swift
+++ Tuist/ProjectDescriptionHelpers/Helper/AppConfiguration.swift
@@ -33,31 +33,6 @@ public extension XCConfig {
 }
 
 public extension Configuration {
-  static let debug: Self = .debug(name: .debug, settings: .provisioning(buildMode: .debug),  xcconfig: XCConfig.Application.app(.debug))
-  static let release: Self = .release(name: .release, settings: .provisioning(buildMode: .release), xcconfig: XCConfig.Application.app(.release))
-}
-
-extension ProjectDescription.SettingsDictionary {
-  enum BuildMode {
-    case debug
-    case release
-    
-    var provisioningName: String {
-      switch self {
-      case .debug:
-        return "Development"
-      case .release:
-        return "AppStore"
-      }
-    }
-  }
-  
-  static func provisioning(buildMode: BuildMode) -> SettingsDictionary {
-    [
-      "DEVELOPMENT_TEAM": "SJDR485DTV",
-      "CODE_SIGN_STYLE": "Manual",
-      "PROVISIONING_PROFILE_SPECIFIER": "match \(buildMode.provisioningName) com.tht.demo.fallingdemoapp",
-      "CODE_SIGN_IDENTITY": "Apple Distribution: Kangho lee (UY52N5ZAAV)"
-    ]
-  }
+  static let debug: Self = .debug(name: .debug, xcconfig: XCConfig.Application.app(.debug))
+  static let release: Self = .release(name: .release, xcconfig: XCConfig.Application.app(.release))
 }
